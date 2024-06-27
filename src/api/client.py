b8a1db90e3db0f8e353f538aeb8c27a968af8da6
Jonathan Cohen <jon@Jonathans-MacBook-Pro.local>
2024-06-27 12:22:17 -0400

    lil clean up

--- src/api/client.py
+++ src/api/client.py
@@ -7,23 +7,6 @@ from src.api.endpoints import HEADERS, ENCOUNTERS_QUERY_PARAMS, TIMEOUT, RETRY_C
 
 
 class APIClient:
-    # @staticmethod
-    # def get_encounters():
-    #     query_url = f"{ENCOUNTERS_ENDPOINT}"
-    #     print(f"Requesting encounter data...")
-    #     response = requests.get(query_url, headers=HEADERS, timeout=TIMEOUT, params=ENCOUNTERS_QUERY_PARAMS)
-    #     print(f"Generated URL: {response.url}")
-    #     try:
-    #         response.raise_for_status()
-    #     except requests.exceptions.HTTPError as error:
-    #         print(f"HTTP error occurred: {error}")
-    #         print(f"Response text: {response.text}")
-    #         raise
-    #     encounters = response.json()
-    #     return encounters if encounters else None
-
-    # pull headers from config file
-    # pull timeout from config file
     @staticmethod
     def make_request(query_url: str, params: Dict[str, any] = None) -> Optional[requests.Response]:
         print(f"Requesting data...")
